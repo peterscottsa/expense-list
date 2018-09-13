6225eb44d1460902e3c7c80e103c8a00d21f892f
Peter Scott <peterscott.sa@gmail.com>
2018-09-13 22:20:14 +0200

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import Expenses from './containers/Expenses'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <h1>Hi</h1>
+        <Expenses fetchOnMount>
+          { ({ allIds, expensesById }) =>
+            allIds.map( id =>
+              <p key={id}>
+                { expensesById[id].amount.value } { expensesById[id].amount.currency }
+              </p>
+            )
+          }
+        </Expenses>
       </Provider>
     )
   }
