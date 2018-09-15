18bc6507e7acbea49b9fa401f3e46564c6730095
Peter Scott <peterscott.sa@gmail.com>
2018-09-15 20:58:18 +0200

    Remove unused param

--- src/redux/sagas/expenses.js
+++ src/redux/sagas/expenses.js
@@ -2,7 +2,7 @@ import { takeLatest, call, put } from 'redux-saga/effects'
 import { expensesModule } from '../modules'
 import { fetchExpenses, addCommentToExpense } from '../api'
 
-function* fetch({ payload }) {
+function* fetch() {
   const response = yield call(fetchExpenses)
   yield put(expensesModule.fetchSuccess(response.data.expenses))
 }
