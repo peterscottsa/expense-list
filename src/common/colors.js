d0dcd1ef8e2fcdbc158afd6012b157db15187899
Peter Scott <peterscott.sa@gmail.com>
2018-09-15 17:21:42 +0200

    - Add google font to index file - Create global styles for colors and spacing - Add global rules to html and body for fonts

---
+++ src/common/colors.js
@@ -0,0 +1,7 @@
+export default {
+  veniceBlue: '#05668D',
+  blueLagoon: '#028090',
+  persianGreen: '#00A896',
+  caribbean: '#02C39A',
+  tusk: '#F0F3BD'
+}
---
+++ src/common/spacing.js
@@ -0,0 +1,9 @@
+export default {
+  xxsmall: 3,
+  xsmall: 5,
+  small: 8,
+  medium: 12,
+  regular: 18,
+  large: 24,
+  gigantic: 48
+}
